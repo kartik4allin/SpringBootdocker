9a8a282356f5d8394677abdaf7419b48790b4282
kartik <kartik4allin@gmail.com>
2020-02-09 20:30:02 -0500

    commiting changes

--- src/main/java/com/example/docker/SpringbootDocker/controllers/SprintBootDockerController.java
+++ src/main/java/com/example/docker/SpringbootDocker/controllers/SprintBootDockerController.java
@@ -9,7 +9,7 @@ public class SprintBootDockerController {
 	
 	@GetMapping
 	public String hello() {
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 6; i++) {
 		
 			System.out.println("Print message for 5 times");
 			
